4fb2549a2b18844e1be1afe014abf1de9c806c59
Iwo <iwoskater3@hotmail.com>
2021-12-15 19:19:16 +0100

    first commit

---
+++ desktop/src/com/iwo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.iwo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.iwo.game.IwoDrop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IwoDrop(), config);
+	}
+}
